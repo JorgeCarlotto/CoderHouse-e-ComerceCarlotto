259cee4c9dbe7410190ec0ac31454d316db65169
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-02-17 11:58:54 -0300

    firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkZasCyVALvgm2MfIV9sLTOEMS52r5ERQ",
+  authDomain: "betta-shop-ecommerce.firebaseapp.com",
+  projectId: "betta-shop-ecommerce",
+  storageBucket: "betta-shop-ecommerce.appspot.com",
+  messagingSenderId: "761955250691",
+  appId: "1:761955250691:web:fe3eda4bd267c697095436"
+};
+
+
+initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
