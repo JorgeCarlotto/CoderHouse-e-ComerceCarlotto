888694997cf8f80ff64fb7296987fdb3077ff8bd
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-02-23 09:44:19 -0300

    delete data and hooks

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import useProducts from "../../hooks/useProducts";
+
 import ItemDetail from "../Item/ItemDetail";
 
 import {collection,getDocs} from 'firebase/firestore'
