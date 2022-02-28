db6b58c44274689af77c4d76d04e79a71ba787e2
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-02-28 08:30:58 -0300

    Checkout Ok

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Cart from '../components/Cart/Cart';
+import { Checkout } from '../components/Checkout/Checkout';
 import Footer from '../components/Footer/Footer';
 import ItemDetailContainer from "../components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "../components/ItemListContainer/ItemListContainer"
 import NavBar from '../components/NavBar/NavBar';
 import NotFound from "../components/NotFound/NotFound";
+
 
 const Rout = ()=>{
     return (
@@ -19,6 +21,7 @@ const Rout = ()=>{
             <Route path="/category/:id" element={<ItemListContainer />} />
             <Route path="/item/:id" element={<ItemDetailContainer />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element ={<Checkout/>}/> 
             <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer/>
